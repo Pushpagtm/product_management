9e76c4d892176c5a37930567e211ecf4c5d45f67
Pushpagtm <pushpagautam529@gmail.com>
2023-07-03 22:58:33 +0545

    work on landing page

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../features/navbar/Navbar';
+import Header from '../features/Header/Header';
+import ProductList from '../features/product/components/ProductList';
+
+function Home(props) {
+    return (
+        <>
+         <Navbar/>
+        <Header/>
+        <ProductList/>
+        </>
+   
+    );
+}
+
+export default Home;
